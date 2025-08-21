9bafdae14041746208778ee33357229ce0fe1fee
JaoVCarvalho <jaovcarvalho1@gmail.com>
2025-08-21 14:33:42 -0300

    chore: initialize DietForge (Spring Boot)

---
+++ src/test/java/io/github/jaovcarvalho/dietforge/DietForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jaovcarvalho.dietforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
